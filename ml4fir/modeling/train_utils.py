658fe9f02bef3b0446cc0a00f3828528f0487103
JotaFan <jotaflame@gmail.com>
2025-04-04 17:43:46 +0100

    refactor: merge supervised learning functions into one

--- ml4fir/modeling/train_utils.py
+++ ml4fir/modeling/train_utils.py
@@ -24,20 +24,16 @@ os.makedirs(roc_plot_path, exist_ok=True)
 os.makedirs(confusion_matrix_plot_path, exist_ok=True)
 
 
-def calculate_metrics(y_test, y_pred, y_prob, model, x_train, model_type):
+def calculate_metrics(y_test, y_pred):
     """
-    Calculate evaluation metrics and feature importances.
+    Calculate evaluation metrics.
 
     Parameters:
         y_test (array-like): Ground truth labels.
         y_pred (array-like): Predicted labels.
-        y_prob (array-like): Predicted probabilities.
-        model (object): Trained model.
-        x_train (array-like): Training data.
-        model_type (str): Type of the model ('xgboost' or other).
 
     Returns:
-        tuple: A dictionary containing calculated metrics and an array of feature importances.
+        dict: A dictionary containing calculated metrics.
     """
     metrics = {
         "test_acc": balanced_accuracy_score(y_test, y_pred),
@@ -48,7 +44,21 @@ def calculate_metrics(y_test, y_pred, y_prob, model, x_train, model_type):
         "acc": accuracy_score(y_test, y_pred),
         "roc_auc": None,  # Placeholder for ROC AUC, calculated later
     }
+    return metrics
 
+
+def calculate_feature_importances(model, x_train, model_type):
+    """
+    Calculate feature importances for the given model.
+
+    Parameters:
+        model (object): Trained model.
+        x_train (array-like): Training data.
+        model_type (str): Type of the model ('xgboost' or other).
+
+    Returns:
+        np.ndarray: An array of feature importances.
+    """
     if model_type == "xgboost":
         booster = model.get_booster()
         importance_dict = booster.get_score(importance_type="gain")
@@ -61,7 +71,7 @@ def calculate_metrics(y_test, y_pred, y_prob, model, x_train, model_type):
     else:
         lv_importance = model.feature_importances_
 
-    return metrics, lv_importance
+    return lv_importance
 
 
 def generate_plots(
@@ -117,7 +127,7 @@ def generate_plots(
     )
 
 
-def supervised_training_grid(
+def supervised_training_search(
     x_train,
     y_train,
     x_test,
@@ -132,10 +142,11 @@ def supervised_training_grid(
     target_column,
     back_projection,
     model_type,
+    search_type="grid",  # "grid" for GridSearchCV, "bayes" for BayesSearchCV
     group_fam_to_use=None,
 ):
     """
-    Perform supervised training using GridSearchCV.
+    Perform supervised training using either GridSearchCV or BayesSearchCV.
 
     Parameters:
         x_train (array-like): Training features.
@@ -152,6 +163,7 @@ def supervised_training_grid(
         target_column (str): Target column name.
         back_projection (dict): Back projection dictionary to update.
         model_type (str): Type of the model.
+        search_type (str): Type of search to perform ("grid" or "bayes").
         group_fam_to_use (optional): Optional grouping family.
 
     Returns:
@@ -159,21 +171,40 @@ def supervised_training_grid(
     """
     config = get_model_config(model_type)
     model_name = config.desc_name
-    param_grid = config.get_param_grid()
-    model_args = model_args_conf.get(model_type, {}).get("GridSearchCV", {})
-    model = config.get_model(**model_args)
-    grid_search_params = search_args[config.name]["GridSearchCV"]
-    grid_search = GridSearchCV(model, param_grid, **grid_search_params)
-    grid_search.fit(x_train, y_train)
-    best_model = grid_search.best_estimator_
 
+    if search_type == "grid":
+        search_function_name = "GridSearchCV"
+        search_fn = GridSearchCV
+        param_grid = config.get_param_grid()
+    elif search_type == "bayes":
+        search_function_name = "BayesSearchCV"
+        search_fn = BayesSearchCV
+        param_grid = config.get_bayes_search_params()
+        param_grid.update({"random_state": config.random_seed})
+    else:
+        raise ValueError("Invalid search_type. Choose 'grid' or 'bayes'.")
+
+    # Get model and search parameters
+    model_args = model_args_conf.get(model_type, {}).get(search_function_name, {})
+    model = config.get_model(**model_args)
+
+    search_params = search_args[config.name][search_function_name]
+    search = search_fn(model, param_grid, **search_params)
+    # Fit the search
+    search.fit(x_train, y_train)
+    best_model = search.best_estimator_
+
+    # Make predictions
     y_pred = best_model.predict(x_test)
     y_prob = best_model.predict_proba(x_test)
 
-    metrics, lv_importance = calculate_metrics(
-        y_test, y_pred, y_prob, best_model, x_train, model_type
-    )
+    # Calculate metrics
+    metrics = calculate_metrics(y_test, y_pred)
 
+    # Calculate feature importances
+    lv_importance = calculate_feature_importances(best_model, x_train, model_type)
+
+    # Perform back projection
     top_wavenumbers, top_importances = func_back_projection(
         lv_importance,
         loadings,
@@ -182,10 +213,11 @@ def supervised_training_grid(
         target_column,
         sample_type,
         train_percentage,
-        f"{model_name} (GridSearchCV)",
+        f"{model_name} ({'GridSearchCV' if search_type == 'grid' else 'BayesOpt'})",
         group_fam_to_use=group_fam_to_use,
     )
 
+    # Update cross-validation results
     cross_validation_results = func_cv_results(
         cross_validation_results,
         sample_type,
@@ -195,11 +227,12 @@ def supervised_training_grid(
         metrics["recall"],
         metrics["precision"],
         metrics["cm"],
-        grid_search,
+        search,
         metrics["acc"],
-        f"{model_name} (GridSearchCV)",
+        f"{model_name} ({'GridSearchCV' if search_type == 'grid' else 'BayesOpt'})",
     )
 
+    # Generate plots
     generate_plots(
         y_test,
         y_pred,
@@ -207,131 +240,17 @@ def supervised_training_grid(
         label_encoder,
         sample_type,
         train_percentage,
-        f"{model_name} (GridSearchCV)",
+        f"{model_name} ({'GridSearchCV' if search_type == 'grid' else 'BayesOpt'})",
         target_column,
         group_fam_to_use,
     )
 
+    # Update results
     results, back_projection = results_func(
         results,
         sample_type,
         train_percentage,
-        f"{model_name} (GridSearchCV)",
-        metrics["test_acc"],
-        metrics["f1"],
-        metrics["roc_auc"],
-        top_wavenumbers,
-        top_importances,
-        back_projection,
-    )
-
-    return results, cross_validation_results, back_projection
-
-
-def supervised_training_bayes(
-    x_train,
-    y_train,
-    x_test,
-    y_test,
-    label_encoder,
-    sample_type,
-    train_percentage,
-    loadings,
-    wavenumbers,
-    results,
-    cross_validation_results,
-    target_column,
-    back_projection,
-    model_type,
-    group_fam_to_use=None,
-):
-    """
-    Perform supervised training using BayesSearchCV.
-
-    Parameters:
-        x_train (array-like): Training features.
-        y_train (array-like): Training labels.
-        x_test (array-like): Testing features.
-        y_test (array-like): Testing labels.
-        label_encoder (object): Label encoder for decoding labels.
-        sample_type (str): Type of the sample.
-        train_percentage (float): Training percentage.
-        loadings (array-like): Loadings for back projection.
-        wavenumbers (array-like): Wavenumbers for back projection.
-        results (dict): Results dictionary to update.
-        cross_validation_results (dict): Cross-validation results dictionary to update.
-        target_column (str): Target column name.
-        back_projection (dict): Back projection dictionary to update.
-        model_type (str): Type of the model.
-        group_fam_to_use (optional): Optional grouping family.
-
-    Returns:
-        tuple: Updated results, cross-validation results, and back projection.
-    """
-    config = get_model_config(model_type)
-    model_name = config.desc_name
-
-    model_args = model_args_conf.get(model_type, {}).get("BayesSearchCV", {})
-    model = config.get_model(**model_args)
-
-    bayes_search_params = config.get_bayes_search_params()
-    bayes_search_args = search_args[config.name]["BayesSearchCV"]
-    bayes_search = BayesSearchCV(
-        model, bayes_search_params, random_state=config.random_seed, **bayes_search_args
-    )
-    bayes_search.fit(x_train, y_train)
-    best_bayes = bayes_search.best_estimator_
-
-    y_pred = best_bayes.predict(x_test)
-    y_prob = best_bayes.predict_proba(x_test)
-
-    metrics, lv_importance = calculate_metrics(
-        y_test, y_pred, y_prob, best_bayes, x_train, model_type
-    )
-
-    top_wavenumbers, top_importances = func_back_projection(
-        lv_importance,
-        loadings,
-        wavenumbers,
-        metrics["test_acc"],
-        target_column,
-        sample_type,
-        train_percentage,
-        f"{model_name} (BayesOpt)",
-        group_fam_to_use=group_fam_to_use,
-    )
-
-    cross_validation_results = func_cv_results(
-        cross_validation_results,
-        sample_type,
-        train_percentage,
-        metrics["test_acc"],
-        metrics["f1"],
-        metrics["recall"],
-        metrics["precision"],
-        metrics["cm"],
-        bayes_search,
-        metrics["acc"],
-        f"{model_name} (BayesOpt)",
-    )
-
-    generate_plots(
-        y_test,
-        y_pred,
-        y_prob,
-        label_encoder,
-        sample_type,
-        train_percentage,
-        f"{model_name} (BayesOpt)",
-        target_column,
-        group_fam_to_use,
-    )
-
-    results, back_projection = results_func(
-        results,
-        sample_type,
-        train_percentage,
-        f"{model_name} (BayesOpt)",
+        f"{model_name} ({'GridSearchCV' if search_type == 'grid' else 'BayesOpt'})",
         metrics["test_acc"],
         metrics["f1"],
         metrics["roc_auc"],
@@ -359,6 +278,7 @@ def supervised_training(
     back_projection,
     model_type,
     group_fam_to_use=None,
+    search_to_use=None,  # "grid" for GridSearchCV, "bayes" for BayesSearchCV
 ):
     """
     Train a supervised model based on the specified model_type.
@@ -384,41 +304,30 @@ def supervised_training(
     Returns:
         tuple: Updated results, cross-validation results, and back projection.
     """
-    results, cross_validation_results, back_projection = supervised_training_grid(
-        x_train,
-        y_train,
-        x_test,
-        y_test,
-        label_encoder,
-        sample_type,
-        train_percentage,
-        loadings,
-        wavenumbers,
-        results,
-        cross_validation_results,
-        target_column,
-        back_projection,
-        model_type,
-        group_fam_to_use,
-    )
+    search_to_use = search_to_use or ["grid", "bayes"]
+    if not isinstance(search_to_use, list):
+        search_to_use = [search_to_use]
+
+    for search_type in search_to_use:
+        results, cross_validation_results, back_projection = supervised_training_search(
+            x_train,
+            y_train,
+            x_test,
+            y_test,
+            label_encoder,
+            sample_type,
+            train_percentage,
+            loadings,
+            wavenumbers,
+            results,
+            cross_validation_results,
+            target_column,
+            back_projection,
+            model_type,
+            group_fam_to_use=group_fam_to_use,
+            search_type=search_type,
+        )
 
     # WHY: why return the results etc if they are overwritten?
-    results, cross_validation_results, back_projection = supervised_training_bayes(
-        x_train,
-        y_train,
-        x_test,
-        y_test,
-        label_encoder,
-        sample_type,
-        train_percentage,
-        loadings,
-        wavenumbers,
-        results,
-        cross_validation_results,
-        target_column,
-        back_projection,
-        model_type,
-        group_fam_to_use,
-    )
 
     return results, cross_validation_results, back_projection
