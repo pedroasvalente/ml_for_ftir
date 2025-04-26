43935257af106c32b112fc08a4ed023efd35f92d
JotaFan <jotaflame@gmail.com>
2025-04-26 21:01:43 +0100

    chore: surpress mlflow logging

--- ml4fir/cli.py
+++ ml4fir/cli.py
@@ -1,18 +1,21 @@
 import typer
+
+from ml4fir.config import logger
 from ml4fir.modeling.train import train as train_main
 
 app = typer.Typer()
+
 
 @app.command()
 def train(
     experiment_config: str = typer.Argument(
         ..., help="Path to the experiment configuration file."
     )
-    ):
+):
     """
     Run the training script.
     """
-    typer.echo(f"Running training with config: {experiment_config}")
+    logger.info(f"Running training with config: {experiment_config}")
     train_main(experiment_config=experiment_config)
 
 
@@ -21,6 +24,4 @@ def predict():
     """
     Prediction using trained model
     """
-    typer.echo("Prediction using trained model")
-
-
+    logger.info("Prediction using trained model")
