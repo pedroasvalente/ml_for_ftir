21f89e7069071b4d51d382c469eaee57213dcd43
Pedro Afonso Valente <pedrovalente505@gmail.com>
2025-05-03 10:28:00 +0100

    plots: only try to mlflow log plot when mlflow is running

--- tests/ploting/test_plots.py
+++ tests/ploting/test_plots.py
@@ -1,48 +1,84 @@
+import os
+
 import numpy as np
 
+from ml4fir.config import TESTS_DIR
 from ml4fir.ploting import (
     plot_confusion_matrix,
     plot_roc_curve,
     plot_wavenumber_importances,
 )
-from ml4fir.config import TESTS_DIR
+
 
 def test_plot_confusion_matrix():
-    npz_path = os.path.join(TESTS_DIR, "ploting", "test_data", "plot_confusion_matrix.npz")
+    npz_path = os.path.join(
+        TESTS_DIR, "ploting", "test_data", "plot_confusion_matrix.npz"
+    )
     npz = np.load(npz_path, allow_pickle=True)
     npz_dict = dict(npz)
-    for key in [   'accuracy_score',
-        'sample_type', 'train_percentage', 'test_name', 'target_name',
-        'threshold', 'group_fam_to_use']:
+    for key in [
+        "accuracy_score",
+        "sample_type",
+        "train_percentage",
+        "test_name",
+        "target_name",
+        "threshold",
+        "group_fam_to_use",
+    ]:
         npz_dict[key] = npz_dict[key].item()
-    npz_dict['accuracy_score'] = 0.75
+    npz_dict["accuracy_score"] = 0.75
 
-    plot_confusion_matrix(plot_filepath=npz_path.replace(".npz", ".png"), **npz_dict)
+    plot_confusion_matrix(
+        plot_filepath=npz_path.replace(".npz", ".png"),
+        mlflow_is_running=False,
+        **npz_dict,
+    )
 
 
 def test_plot_roc_curve():
     npz_path = os.path.join(TESTS_DIR, "ploting", "test_data", "plot_roc_curve.npz")
     npz = np.load(npz_path, allow_pickle=True)
     npz_dict = dict(npz)
-    for key in [ 'sample_type', 'train_percentage', 'test_name', 'target_name',
-        'threshold', 'group_fam_to_use' ,"test_accuracy"]:
+    for key in [
+        "sample_type",
+        "train_percentage",
+        "test_name",
+        "target_name",
+        "threshold",
+        "group_fam_to_use",
+        "test_accuracy",
+    ]:
         npz_dict[key] = npz_dict[key].item()
-    npz_dict['test_accuracy'] = 0.75
+    npz_dict["test_accuracy"] = 0.75
 
-    plot_roc_curve(plot_filepath=npz_path.replace(".npz", ".png"), **npz_dict)
+    plot_roc_curve(
+        plot_filepath=npz_path.replace(".npz", ".png"),
+        mlflow_is_running=False,
+        **npz_dict,
+    )
 
 
 def test_plot_wavenumber_importances():
-    npz_path = os.path.join(TESTS_DIR, "ploting", "test_data", "plot_wavenumber_importances.npz")
+    npz_path = os.path.join(
+        TESTS_DIR, "ploting", "test_data", "plot_wavenumber_importances.npz"
+    )
     npz = np.load(npz_path, allow_pickle=True)
     npz_dict = dict(npz)
-    for key in [ 'sample_type', 'train_percentage', 'test_name',
-    'target_name',
+    for key in [
+        "sample_type",
+        "train_percentage",
+        "test_name",
+        "target_name",
         "group_suffix",
-        ]:
+    ]:
         npz_dict[key] = npz_dict[key].item()
 
-    plot_wavenumber_importances(plot_filepath=npz_path.replace(".npz", ".png"), **npz_dict)
+    plot_wavenumber_importances(
+        plot_filepath=npz_path.replace(".npz", ".png"),
+        mlflow_is_running=False,
+        **npz_dict,
+    )
+
 
 test_plot_confusion_matrix()
 test_plot_roc_curve()
